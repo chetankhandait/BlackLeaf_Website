bccc3825f268afa13e5979a132afd700a490d88d
chetankhandait <chetankhandait16@gmail.com>
2024-04-07 21:36:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h3 className='bg-blue-400 p-3'>
+        hi this chetan
+      </h3>
+    </div>
+  )
+}
+
+export default App
