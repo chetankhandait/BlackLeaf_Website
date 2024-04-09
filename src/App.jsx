a5562f8590740197e16fd4f6f679122c36dad09b
chetankhandait <chetankhandait16@gmail.com>
2024-04-09 18:27:27 +0530

    added routes for seprate page add sepratepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from "./Components/Home";
 import LocationSection from "./Components/LocationSection";
+import SepratePage from "./Components/SepratePage";
 
 const App = () => {
   return (
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/sepratepage" element={<LocationSection/>}/>
+        <Route path="/sepratepage" element={<SepratePage/>}/>
       </Routes>
       </BrowserRouter>   
   );
