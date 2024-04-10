ee5e95d91aac5b8476567d30e2fa77e5f51f54e1
chetankhandait <chetankhandait16@gmail.com>
2024-04-10 16:06:56 +0530

    correct route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Hero from "./Components/Hero";
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-import ProductList from "./Components/ProductList";
-import Stat from "./Components/Stat";
-import CardDisplay from "./Components/CardDisplay";
+import Home from "./Components/Home";
+import SepratePage from "./Components/SepratePage";
+
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 
 const App = () => {
   return (
