3814f82dd7164dbef0254d613a953395a75704e3
chiragg987 <cg5031046@gmail.com>
2024-04-11 17:45:17 +0530

    chetan

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Navbar from "./Components/Navbar";
 import ProductList from "./Components/ProductList";
 import Stat from "./Components/Stat";
 import CardDisplay from "./Components/CardDisplay";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home"; // Import your Home component
+import SepratePage from "./Components/SepratePage"; // Import your SepratePage component
 const App = () => {
   return (
     <BrowserRouter>
