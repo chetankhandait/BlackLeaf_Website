b8520f6bd14266a83a4283a3317ca28f942c0526
chiragg987 <cg5031046@gmail.com>
2024-04-11 17:27:00 +0530

    bhai sa

--- src/Components/SepratePage.jsx
+++ src/Components/SepratePage.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
-import Data from "../Data/data"
+import React from "react";
+import { useParams } from "react-router-dom";
+import Data from "../Data/data";
 const SepratePage = () => {
-
- 
   return (
     <div>
-      <h2>
-        Seprate page
-       
-      </h2>
+      <h2>Seprate page</h2>
     </div>
-  )
-}
+  );
+};
 
-export default SepratePage
+export default SepratePage;
