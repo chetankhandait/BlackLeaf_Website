9d0181783d21eca53c9396eefe8d9a1a40c28d2b
chetankhandait <chetankhandait16@gmail.com>
2024-04-18 17:16:08 +0530

    loader addd

--- src/Components/SepratePage.jsx
+++ src/Components/SepratePage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Data from "../Data/data";
 import { FeaturedImageGallery } from "./FeaturedImageGallery";
 import { ListDefault } from "./ListDefault";
-import Content from "./Content";
+// import Content from "./Content";
 const SepratePage = () => {
   return (
     <div>
