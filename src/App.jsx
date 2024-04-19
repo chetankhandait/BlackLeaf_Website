b1e217e5dc4019b30868bdc6eafec4f3d4f7dc04
chetankhandait <chetankhandait16@gmail.com>
2024-04-19 10:57:43 +0530

    route fix

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/sepratepage/:title" element={<SepratePage />} />
+              <Route path="/sepratepage" element={<SepratePage />} />
             </Routes>
           </BrowserRouter>
         )
