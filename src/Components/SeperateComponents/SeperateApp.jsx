1b7647948972098d2506a9c3cc4fb70d32a5cf11
chiragg987 <cg5031046@gmail.com>
2024-04-20 13:04:50 +0530

    sticky form

--- src/Components/SeperateComponents/SeperateApp.jsx
+++ src/Components/SeperateComponents/SeperateApp.jsx
@@ -7,13 +7,17 @@ import Overview from "./Overview";
 
 function SeperateApp() {
   return (
-    <div className="text-white">
-      <NavbarSeperate />
-      <AboutSeperate />
-      <Overview />
-      <FloorPlan />
-      <Locality />
-      <Form />
+    <div className="text-white flex justify-center">
+      <div className="flex flex-col items-start">
+        <NavbarSeperate />
+        <AboutSeperate />
+        <Overview />
+        <FloorPlan />
+        <Locality />
+      </div>
+      <div className=" h-full sticky top-0 flex flex-col items-end">
+        <Form />
+      </div>
     </div>
   );
 }
